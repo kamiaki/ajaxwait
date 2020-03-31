6e50f4224caef0907bdaae53ea08e78aac20d999
kamiaki <247557768@qq.com>
2020-03-31 11:22:37 +0800

    wait方式

--- src/main/java/com/aki/ajaxwait/AjaxController.java
+++ src/main/java/com/aki/ajaxwait/AjaxController.java
@@ -3,33 +3,51 @@ package com.aki.ajaxwait;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @RestController
 public class AjaxController {
     public String Suo = "";
-    public ConcurrentHashMap map = new ConcurrentHashMap();
+    private static Lock lock = new ReentrantLock();
+    public static ConcurrentHashMap map = new ConcurrentHashMap();
 
-
+    //http://192.168.2.15:8077/sso/testWait
     @RequestMapping(value = "testWait")
-    public String testWait(String uuid){
-        map.put(uuid, false);
-        while (true){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if((boolean)map.get(uuid) == true){
-                break;
-            }
+    public String testWait(String uuid, HttpServletRequest request){
+        String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/qrcodelogin/notifyRequest/";
+        System.out.println(basePath);
+
+        Condition condition = lock.newCondition();
+        map.put(uuid, condition);
+
+        this.lock.lock();
+        try {
+            condition.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            this.lock.unlock();
         }
+
         return uuid + "被解锁";
     }
 
     @RequestMapping(value = "open")
     public String open(String uuid){
-        map.put(uuid, true);
+        Condition o = (Condition)map.get(uuid);
+        this.lock.lock();
+        try {
+            o.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            this.lock.unlock();
+        }
+
         return "解锁" + uuid;
     }
 }
